acf2293e4d215f465db9b49b1e9e2051776e87ab
Guanlun Zhao <guanlunzhao@gmail.com>
2014-04-01 00:46:14 +0800

    added buggy depth of field which causes timeout exception

--- phong.cu
+++ phong.cu
@@ -7,7 +7,7 @@
 using namespace optix;
 
 // use offline rendering
-#define OFFLINE
+// #define OFFLINE
 
 // rtDeclareVariable(unsigned int, thread_index, attribute thread_index, );
 rtDeclareVariable(uint2, thread_index, rtLaunchIndex, );
@@ -81,18 +81,24 @@ static __device__ float3 randomizeVector(const float3& v, float amount, unsigned
 	// two random number for normal distribution
 	float rand1 = rnd(seed), rand2 = rnd(seed);
 
+	// X and Y are normally distributed random numbers
 	float X = sqrt(- 2 * log(rand1)) * cos(2 * PI * rand2) * amount;
 	float Y = sqrt(- 2 * log(rand1)) * sin(2 * PI * rand2) * amount;
 
+	// make a vector not parallel to v to find v's tangent and bitangent
 	float3 u = v;
 	u.x += 1;
 
+	// tangent
 	float3 e1 = cross(u, v);
+
+	//bitangent
 	float3 e2 = cross(v, e1);
 
 	normalize(e1);
 	normalize(e2);
 
+	// randomize v in its tangent and bitangent direction
 	float3 rand_vec = v + e1 * X + e2 * Y;
 	normalize(rand_vec);
 
@@ -120,23 +126,26 @@ RT_PROGRAM void any_hit_shadow()
 
 RT_PROGRAM void closest_hit_radiance()
 {
-	if (is_emissive) {
-	prd_radiance.result = make_float3(1, 1, 1);
-	return;
+	if (is_emissive) { // emissive object, just return the light color
+		prd_radiance.result = make_float3(1, 1, 1);
+		return;
 	}
+
+	// seed used for random number generation
+	unsigned int seed = tea<16>(thread_index.y * thread_dim.x + thread_index.x, thread_index.y);;
 
 	const float3 ray_dir = ray.direction; 
 	const float3 uvw = texcoord;
 
 	float3 kd;
-	if (has_diffuse_map) {
+	if (has_diffuse_map) { // has diffuse map, sample the texture
 		kd = make_float3( tex2D( kd_map, uvw.x, uvw.y ) );
 	} else {
 		kd = k_diffuse;
 	}
 
 	float3 ks;
-	if (has_specular_map) {
+	if (has_specular_map) { // has specular map, sample the texture
 		ks = make_float3( tex2D( ks_map, uvw.x, uvw.y ) );
 	} else {
 		ks = k_specular;
@@ -144,6 +153,8 @@ RT_PROGRAM void closest_hit_radiance()
 
 	float3 kr = k_reflective;
 
+	// Here tangent, bitangent and normal are attribute variables set in the ray-generation program,
+	// transform them to the work space using the normal transformation matrix
 	const float3 T = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, tangent)); // tangent	
 	const float3 B = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, bitangent)); // bitangent	
 	const float3 N = normalize(rtTransformNormal(RT_OBJECT_TO_WORLD, shading_normal)); // normal	
@@ -151,43 +162,38 @@ RT_PROGRAM void closest_hit_radiance()
 	// normal vector after considering normal map (if any)
 	float3 normal;
 
-	if (has_normal_map) {
+	if (has_normal_map) { // has normal map, sample the texture
 		const float3 k_normal = make_float3( tex2D( normal_map, uvw.x, uvw.y) );
-		const float3 coeff = k_normal * 2 - make_float3(1, 1, 1);
+		const float3 coeff = k_normal * 2 - make_float3(1, 1, 1); // transform from RGB to normal
 		normal = T * coeff.x + B * coeff.y + N * coeff.z;
 	} else {
 		normal = N;
 	}
 
+	// first hit point
 	const float3 fhp = rtTransformPoint(RT_OBJECT_TO_WORLD, front_hit_point);
 
-	//attenuation
-	const float beer_attenuation = 1.0f;
-
 	const int depth = prd_radiance.depth;
+
+	// starting from the ambient color
 	float3 result = kd * ambient_light_color;
-
-	// seed used for random number generation
-	unsigned int seed = tea<16>(thread_index.y * thread_dim.x + thread_index.x, thread_index.y);;
 		
-	// unsigned int num_lights = lights.size();
-	unsigned int num_lights = area_lights.size();
-
-	for (int i = 0; i < num_lights; i++) {
-		// BasicLight light = lights[i];
+	for (int i = 0; i < area_lights.size(); i++) {
 		RectangleLight light = area_lights[i];
 
+		// according to whether offline rendering is activated, use different number of samples to
+		// achieve soft or hard shadow
 #ifdef OFFLINE
-		const int numSamples = 10;
-		
+		const int numShadowSamples = 10;
 #else
-		const int numSamples = 1;
+		const int numShadowSamples = 1;
 #endif
 
-		for (int j = 0; j < numSamples; j++) {
+		for (int j = 0; j < numShadowSamples; j++) {
 #ifdef OFFLINE
-			float2 rp = make_float2(rnd(seed), rnd(seed));
-			float3 sampledPos = light.pos + rp.x * light.r1 + rp.y * light.r2;
+			float2 random_pair = make_float2(rnd(seed), rnd(seed));
+			// randomly sample points in the area light source
+			float3 sampledPos = light.pos + random_pair.x * light.r1 + random_pair.y * light.r2;
 #else
 			float3 sampledPos = light.pos;
 #endif
@@ -196,7 +202,7 @@ RT_PROGRAM void closest_hit_radiance()
 
 			float3 L = normalize(sampledPos - fhp);
 			float3 H = normalize(L - ray.direction);
-			// float nDl = dot(N, L);
+
 			float nDl = dot(normal, L);
 
 			// cast shadow ray
@@ -207,10 +213,13 @@ RT_PROGRAM void closest_hit_radiance()
 				optix::Ray shadow_ray = optix::make_Ray( fhp, L, shadow_ray_type, scene_epsilon, Ldist );
 				rtTrace(top_shadower, shadow_ray, shadow_prd);
 				result += light.color * shadow_prd.attenuation 
-					* (kd * nDl + ks * max(pow(dot(H, normal), ns), .0f)) / numSamples;
+					* (kd * nDl + ks * max(pow(dot(H, normal), ns), .0f)) / numShadowSamples;
 			}
 		}
 	}
+
+	// beer attenuation for reflection
+	const float beer_attenuation = 1.0f;
 
 	float3 refl_color = make_float3(0, 0, 0);
 	float reflection = 1.0f;
@@ -232,7 +241,7 @@ RT_PROGRAM void closest_hit_radiance()
 		}
 		refl_color /= numGlossySample;
 #else
-		refl_color = TraceRay(fhp, r, depth + 1, importance) / float(numGlossySample);
+		refl_color = TraceRay(fhp, r, depth + 1, importance);
 #endif
 	}
 
--- pinhole_camera.cu
+++ pinhole_camera.cu
@@ -2,6 +2,7 @@
 #include <optixu/optixu_math_namespace.h>
 
 #include "helpers.h"
+#include "random.h"
 
 using namespace optix;
 
@@ -44,29 +45,40 @@ __device__ __forceinline__ float3 read_output()
 	return make_float3(output_buffer[launch_index]);
 }
 
+#define PI 3.1415926
+
 RT_PROGRAM void pinhole_camera()
 {
 #ifdef TIME_VIEW
 	clock_t t0 = clock(); 
 #endif
+	unsigned int seed = tea<16>(launch_index.y * launch_dim.x + launch_index.x, launch_index.y);;
+
 	float3 result = make_float3(0, 0, 0);
+	float2 d = make_float2(launch_index) / make_float2(launch_dim) * 2.f - 1.f;
 
-	for (float i = 0; i <= 0.1; i += 0.25) {
-		for (float j = 0; j <= 0.1; j += 0.25) {
-			PerRayData_radiance prd;
-			prd.importance = 1.f;
-			prd.depth = 0;
+	const int numDofSamples = 50;
+	for (int i = 0; i < numDofSamples; i++) {
+		PerRayData_radiance prd;
+		prd.importance = 1.f / numDofSamples;
+		prd.depth = 0;
 
-			float2 d = (make_float2(launch_index) + make_float2(i, j)) / make_float2(launch_dim) * 2.f - 1.f;
-			float3 ray_origin = eye;
-			float3 ray_direction = normalize(d.x * U + d.y * V + W);
+		// randomly sample eye positions on a disk
+		float rand_dist = rnd(seed) * 0.01;
+		float rand_angle = rnd(seed) * 2 * PI;
 
-			optix::Ray ray = optix::make_Ray(ray_origin, ray_direction, radiance_ray_type, scene_epsilon, RT_DEFAULT_MAX);
+		float rand_x = rand_dist * cos(rand_angle);
+		float rand_y = rand_dist * sin(rand_angle);
 
-			rtTrace(top_object, ray, prd);
+		// float3 ray_origin = eye + make_float3(0, 0, 0.1 * i);
+		float3 ray_origin = eye + V * rand_x + U * rand_y;
+		float3 ray_direction = normalize(d.x * U + d.y * V + W * 2);
 
-			result += prd.result;
-		}
+		optix::Ray ray = optix::make_Ray(ray_origin, ray_direction, radiance_ray_type, scene_epsilon, RT_DEFAULT_MAX);
+
+		rtTrace(top_object, ray, prd);
+
+		result += prd.result / numDofSamples;
 	}
 
 #ifdef TIME_VIEW
